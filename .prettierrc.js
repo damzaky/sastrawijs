dbe52134047756ab6054b955f0948db9f55584f4
Ananda Umamil <zweimach@zweimach.org>
2020-12-11 22:49:10 +0700

    Move configs to package.json

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  endOfLine: "lf",
-  printWidth: 80,
-  semi: true,
-  trailingComma: "es5",
-};
--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  testMatch: ["<rootDir>/test/**/*.js"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.js"],
-};
