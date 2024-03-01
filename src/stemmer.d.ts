1b9ab74218cfe2e20924477e0071da7dc3beb74c
Damar Zaky <dmzsylvester@gmail.com>
2024-03-01 15:21:26 +0700

    Create stemmer.d.ts

---
+++ src/stemmer.d.ts
@@ -0,0 +1,53 @@
+declare module StemmerModule {
+  type Dictionary = string[];
+
+  export default class Stemmer {
+    internalDictionary: Record<string, string>;
+    vowel: string;
+    consonant: string;
+
+    constructor(dictionary?: Dictionary);
+
+    addToDict(words: string[]): void;
+
+    remove(words: string[]): void;
+
+    hasPrefix(needle: string, haystack: string): boolean;
+
+    find(word: string): boolean;
+
+    print(): Record<string, string>;
+
+    newChar(word: string, index: number): string;
+
+    isOneOf(c: string, chars: string): boolean;
+
+    isNotOneOf(c: string, chars: string): boolean;
+
+    stem(word: string): string;
+
+    removeParticle(word: string): [string, string];
+
+    removePossesive(word: string): [string, string];
+
+    removeSuffix(word: string): [string, string];
+
+    lastReturnLoop(originalWord: string, suffixes: string[]): [boolean, string];
+
+    removePrefixes(word: string): [boolean, string];
+
+    removePrefix(word: string): [string, string, string[]];
+
+    removeMePrefix(word: string): [string, string[] | null];
+
+    removePePrefix(word: string): [string, string[] | null];
+
+    removeBePrefix(word: string): [string, string[] | null];
+
+    removeTePrefix(word: string): [string, string[] | null];
+
+    removeInfix(word: string): [string, [string, string] | null];
+  }
+}
+
+export = StemmerModule;
