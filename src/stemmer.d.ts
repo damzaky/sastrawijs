07502c983090f21bab70e295720b80d4e811b805
Damar Zaky <dmzsylvester@gmail.com>
2024-03-01 15:56:37 +0700

    Update stemmer.d.ts

--- src/stemmer.d.ts
+++ src/stemmer.d.ts
@@ -1,53 +1,50 @@
-declare module StemmerModule {
-  type Dictionary = string[];
+type Dictionary = string[];
 
-  export default class Stemmer {
-    internalDictionary: Record<string, string>;
-    vowel: string;
-    consonant: string;
+export class Stemmer {
+  internalDictionary: Record<string, string>;
+  vowel: string;
+  consonant: string;
 
-    constructor(dictionary?: Dictionary);
+  constructor(dictionary?: Dictionary);
 
-    addToDict(words: string[]): void;
+  addToDict(words: string[]): void;
 
-    remove(words: string[]): void;
+  remove(words: string[]): void;
 
-    hasPrefix(needle: string, haystack: string): boolean;
+  hasPrefix(needle: string, haystack: string): boolean;
 
-    find(word: string): boolean;
+  find(word: string): boolean;
 
-    print(): Record<string, string>;
+  print(): Record<string, string>;
 
-    newChar(word: string, index: number): string;
+  newChar(word: string, index: number): string;
 
-    isOneOf(c: string, chars: string): boolean;
+  isOneOf(c: string, chars: string): boolean;
 
-    isNotOneOf(c: string, chars: string): boolean;
+  isNotOneOf(c: string, chars: string): boolean;
 
-    stem(word: string): string;
+  stem(word: string): string;
 
-    removeParticle(word: string): [string, string];
+  removeParticle(word: string): [string, string];
 
-    removePossesive(word: string): [string, string];
+  removePossesive(word: string): [string, string];
 
-    removeSuffix(word: string): [string, string];
+  removeSuffix(word: string): [string, string];
 
-    lastReturnLoop(originalWord: string, suffixes: string[]): [boolean, string];
+  lastReturnLoop(originalWord: string, suffixes: string[]): [boolean, string];
 
-    removePrefixes(word: string): [boolean, string];
+  removePrefixes(word: string): [boolean, string];
 
-    removePrefix(word: string): [string, string, string[]];
+  removePrefix(word: string): [string, string, string[]];
 
-    removeMePrefix(word: string): [string, string[] | null];
+  removeMePrefix(word: string): [string, string[] | null];
 
-    removePePrefix(word: string): [string, string[] | null];
+  removePePrefix(word: string): [string, string[] | null];
 
-    removeBePrefix(word: string): [string, string[] | null];
+  removeBePrefix(word: string): [string, string[] | null];
 
-    removeTePrefix(word: string): [string, string[] | null];
+  removeTePrefix(word: string): [string, string[] | null];
 
-    removeInfix(word: string): [string, [string, string] | null];
-  }
+  removeInfix(word: string): [string, [string, string] | null];
 }
 
-export = StemmerModule;
